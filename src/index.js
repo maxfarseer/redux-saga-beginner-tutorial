584b82ed611ffef3946e4100e63fc013c8e7b217
Max P <maxfarseer@gmail.com>
2018-08-14 10:07:31 +0300

    refactor (app) delete old files

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { createStore } from 'redux'
 import Counter from './Counter'
 import reducer from './reducers'
 
+import './index.css'
+
 const store = createStore(reducer)
 
 const action = type => store.dispatch({ type })
